cc0b064bec29ee089f4481dd2dde8245dd43881b
Ilaria Bossi <c0diboss1@gmail.com>
2021-05-13 19:28:46 -0400

    'MySecondRealLifeProject!'

--- Automate_The_Boring_Stuff/Chapter7.py
+++ Automate_The_Boring_Stuff/Chapter7.py
@@ -3,5 +3,19 @@ total = 0
 for nums in range(101):
     total = total + nums
 print(total)
+for i in range(0, 10, 2):
+    print('Rainy Day' + str(i))
+
+for i in range(5, -1, -1):
+    print('Rainy Day' + str(i))
+
+
+i = 0 
+while i < 4:
+    print("It's late!")
+    i+=1
+
+for i in range(0, 5, 2):
+    print("It's not late!")
 
     
